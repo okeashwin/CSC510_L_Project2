7bcb0afdfc7f7b602ff34fd451b35c582d0e68c5
Ashwin Oke <okeashwin@gmail.com>
2016-04-29 21:49:24 -0400

    Adding DDL statements to mysql script

--- mysql.py
+++ mysql.py
@@ -14,8 +14,24 @@ cur.execute("SELECT * FROM expense_sharing_expenses")
 print "Connection verified"
 
 # DDL statements for different tables go here
-cur.execute("""CREATE TABLE Issues ( issue_id varchar(100), issue_text varchar(255), 
-			user varchar(50), commit_flag varchar(10), milestone_flag varchar(10), state varchar(10),
-			primary key(issue_id) );""")
+cur.execute("""CREATE TABLE IF NOT EXISTS issue (id INTEGER, name VARCHAR(128),
+        createtime DATETIME NOT NULL,closedtime DATETIME NOT NULL ,milestone_id INTEGER,
+        assignee_id INTEGER,label_str VARCHAR(1024),labels_count INTEGER,
+        CONSTRAINT pk_issue PRIMARY KEY (id) );""")
+cur.execute("""CREATE TABLE IF NOT EXISTS milestone(id INTEGER, title VARCHAR(128), description VARCHAR(1024),
+        created_at DATETIME, due_at DATETIME, closed_at DATETIME, user VARCHAR(128), identifier INTEGER,
+        CONSTRAINT pk_milestone PRIMARY KEY(id));""")
+cur.execute("""CREATE TABLE IF NOT EXISTS event(issueID INTEGER NOT NULL, time DATETIME NOT NULL, action VARCHAR(128),
+        label VARCHAR(128), user VARCHAR(128), milestone INTEGER, identifier INTEGER,
+        CONSTRAINT pk_event PRIMARY KEY (issueID, time, action, label),
+        CONSTRAINT fk_event_issue FOREIGN KEY (issueID) REFERENCES issue(id),
+        CONSTRAINT fk_event_milestone FOREIGN KEY (milestone) REFERENCES milestone(id));""")
+cur.execute("""CREATE TABLE IF NOT EXISTS comment(issueID INTEGER NOT NULL, user VARCHAR(128), createtime DATETIME NOT NULL,
+        updatetime DATETIME, text VARCHAR(1024), identifier INTEGER,
+        CONSTRAINT pk_comment PRIMARY KEY (issueID, user, createtime),
+        CONSTRAINT fk_comment_issue FOREIGN KEY (issueID) REFERENCES issue(id));""")
+cur.execute("""CREATE TABLE IF NOT EXISTS commits(id INTEGER NOT NULL, time DATETIME NOT NULL, sha VARCHAR(128),
+        user VARCHAR(128), message VARCHAR(128),
+        CONSTRAINT pk_commits PRIMARY KEY (id));""")
 
 db.close()
